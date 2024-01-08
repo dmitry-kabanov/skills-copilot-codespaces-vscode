bff4283e14c51e8f57db9f4fd347d267cdccad51
Dmitry Kabanov, PhD <kabanov.dmitry@gmail.com>
2024-01-08 17:36:30 +0000

    Add skills.js

---
+++ skills.js
@@ -0,0 +1,7 @@
+function calculateNumbers(num1, num2) {
+    const sum = num1 + num2;
+    const diff = num1 - num2;
+    const mult = num1 * num2;
+    const div = num1 / num2;
+    return [sum, diff, mult, div];
+}
